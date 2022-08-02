33a1e59c64759bae04d6860e4eb5466565c74e12
Kastel-Mihalson <ConstAnto147@yandex.ru>
2022-08-02 08:18:12 +0300

    main. Удаление лишнего проекта

--- KD.ATM.Machine_task5/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KD.ATM.Machine_task5.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
